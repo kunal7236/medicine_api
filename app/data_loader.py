33f16a18ebf3f6a455289a55edf050604192bf07
kunal7236 <kunakashyap277@gmail.com>
2025-07-31 20:13:33 +0530

    fixed Sr.no field using sanitized function

--- app/data_loader.py
+++ app/data_loader.py
@@ -1,4 +1,3 @@
-#import json
 from datetime import datetime
 from typing import List, Dict
 from pymongo import MongoClient
@@ -7,16 +6,22 @@ from dotenv import load_dotenv
 
 load_dotenv()
 MONGO_URI = os.getenv("MONGO_URI")
-#DATA_FILE = "app/data.json"
+
 
 client = MongoClient(MONGO_URI)
 db = client["medicineDB"]
 collection = db["medicines"]
 meta = db["metadata"]
 
+def sanitize_keys(obj):
+    if isinstance(obj, dict):
+        return {k.replace('.', '_'): sanitize_keys(v) for k, v in obj.items()}
+    elif isinstance(obj, list):
+        return [sanitize_keys(item) for item in obj]
+    else:
+        return obj
+
 def load_data() -> Dict:
-    # with open(DATA_FILE, "r", encoding="utf-8") as f:
-    #     return json.load(f)
     updated = meta.find_one({"_id": "medicines_update"})
     updated_at = updated["updated_at"] if updated else None
     medicines = list(collection.find({}, {"_id": 0}))
@@ -28,14 +33,10 @@ def load_data() -> Dict:
 def save_data(medicines: List[Dict], updated_at: str = None):
     if updated_at is None:
         updated_at = datetime.now().isoformat()
-    # with open(DATA_FILE, "w", encoding="utf-8") as f:
-    #     json.dump({
-    #         "updated_at": updated_at,
-    #         "medicines": medicines
-    #     }, f, indent=2)
     collection.delete_many({})
     if medicines:
-        collection.insert_many(medicines)
+        sanitized_data = [sanitize_keys(entry) for entry in data]
+        collection.insert_many(sanitized_data)
 
     meta.update_one(
         {"_id": "medicines_update"},
